8deff8ee0995f8878a6e5832d0671b1d99e10ea5
Guilhermemrs <guilherme.msantos@aluno.faculdadeimpacta.com.br>
2020-10-09 08:19:44 -0300

    DevopsAc3

---
+++ primos.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(_name_)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+
+    #Faz essa bagaça palhaço
+
+
+    return primos
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
